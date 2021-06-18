9e846b864b87755704fd6cbfadf6a01df703e4db
takeyasukatsunori <yasuo0608@icloud.com>
2021-06-18 17:32:53 +0900

    ダウンロードページの作成

--- admin.php
+++ admin.php
@@ -388,7 +388,14 @@ article.reply::before {
 <section>
 
 <?php if( !empty($_SESSION['admin_login']) && $_SESSION['admin_login'] === true ): ?>
-
+    <form method="get" action="./download.php">
+    <select name="limit">
+        <option value="">全て</option>
+        <option value="10">10件</option>
+        <option value="30">30件</option>
+    </select>
+    <input type="submit" name="btn_download" value="ダウンロード">
+</form>
 <?php if( !empty($message_array) ){ ?>
 <?php foreach( $message_array as $value ){ ?>
 <article>
